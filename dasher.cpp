3e543cb0a5fea2bfc80d92a1f48ac15d69958b29
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-02-14 16:44:00 +0100

    add hazard

--- dasher.cpp
+++ dasher.cpp
@@ -6,32 +6,54 @@ int main()
     const int windowHeight {380};
     InitWindow(windowWidth,windowHeight,"Dapper dasher");
 
+    // nebula variables
+    Texture2D nebula {LoadTexture("textures/12_nebula_spritesheet.png")};
+    Rectangle nebulaRec
+    {
+        0.0,
+        0.0,
+        nebula.width/8,
+        nebula.height/8
+    };
+    Vector2 nebulaPos {windowWidth, windowHeight - nebulaRec.height};
+    
+    // velocity in pixels/s
+    int nebulaVelocity {-600};
+
     // tecture for character setup
     Texture2D scarfy {LoadTexture("textures/scarfy.png")};
-    Rectangle scarfyRec;
-    scarfyRec.width = scarfy.width/6;
-    scarfyRec.height = scarfy.height;
-    scarfyRec.x = 0;
-    scarfyRec.y = 0;
-    Vector2 scarfyPos;
-    scarfyPos.x = windowWidth/2 - scarfyRec.width/2;
-    scarfyPos.y = windowHeight - scarfyRec.height;
+    Rectangle scarfyRec
+    {
+        0.0,
+        0.0,
+        scarfy.width/6,
+        scarfy.height
+    };
+    Vector2 scarfyPos {windowWidth/2 - scarfyRec.width/2, windowHeight - scarfyRec.height};
+
+    // animation frame
+    int frame {0};
+    const float updateTime{1.0 / 12.0};
+    float runningTime{0};
 
     int velocity {0};
-    const int jumpVelocity {-22};
+    const int jumpVelocity {-600};
     bool isInAir {!(scarfyPos.y >= windowHeight - scarfyRec.height)};
 
-    // accelaration pixels/frame
-    const int gravity {1};
+    // accelaration (pixels/frame/s)
+    const int gravity {1'000};
 
+    
     SetTargetFPS(60);
-
     while (!WindowShouldClose())
     {
+        const float deltaTime {GetFrameTime()};
+        
         BeginDrawing();
         ClearBackground(WHITE);
 
         // game logic
+
         
         // ground check
         if (scarfyPos.y >= windowHeight - scarfyRec.height)
@@ -42,7 +64,7 @@ int main()
         else
         {
             // apply gravity
-            velocity += gravity;
+            velocity += gravity * deltaTime;
             isInAir = true;
         }
         
@@ -52,13 +74,38 @@ int main()
             velocity += jumpVelocity;
         }
 
-        // update texture
+        // update nebula texture position
+        nebulaPos.x += nebulaVelocity * deltaTime;
 
-        scarfyPos.y += velocity;
+        // update scarface texture position
+        scarfyPos.y += velocity * deltaTime;
+
+        // update animation frame
+        if (!isInAir)
+        {
+            runningTime += deltaTime;
+            if (runningTime >= updateTime)
+            {
+                runningTime = 0.0;
+                scarfyRec.x = frame * scarfyRec.width;
+                frame++;
+                if (frame > 5)
+                {
+                    frame = 0;
+                }
+            }
+        }
+        
+        
+        // draw nebula
+        DrawTextureRec(nebula, nebulaRec, nebulaPos, WHITE);
+        
+        // draw scarfy
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
         EndDrawing();
     }
     UnloadTexture(scarfy);
+    UnloadTexture(nebula);
     CloseWindow();
 }
